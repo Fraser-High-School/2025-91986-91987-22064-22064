7ec3b8d96a45a50a0b2d3fe9835116bc86bea58a
22064-22064 <22064@fraser.school.nz>
2025-04-08 10:12:12 +1200

    Update blackjack_v1.py
    started the game and gathered the bets as well as create a set of instructions for the user

--- blackjack_v1.py
+++ blackjack_v1.py
@@ -3,6 +3,7 @@ Cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 Suits = ["♥", "♦", "♣", "♠"]
 Player_Deck = []
 Dealer_Deck = []
+Chips = 1000
 
 #function to give cards to players hand
 def Player():
@@ -44,6 +45,21 @@ def Dealer():
         Card_Suit = str(Dealer_Cards) +  random.choice(Suits)
         Dealer_Deck.append(Card_Suit)
 
+#function to check if user enters yes (y) or no (n)
+def Yes_No(question):
+    
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please enter yes (y) or no (n).\n")
+
+
+#while True loop to endlessly make sure there aren't duplicates
 while True:
     #gives the player 2 cards that aren't duplicates
     while len(Player_Deck)<2:
@@ -65,8 +81,55 @@ while True:
     #breaks the loop since there aren't any duplicate cards
     elif Deck == False:
         break
-        
 
-print(*Dealer_Deck)
-print(*Player_Deck)
 
+print("===============================================================")
+print("====================Welcome to Blackjack 21====================")
+print("===============================================================")
+print("")
+
+
+
+Instructions = Yes_No("Would you like to know the rules of Blackjack 21?\n")
+if Instructions == "yes":
+    print("")
+    print("=============================Rules=============================")
+    print("Goal: Get closer to 21 than the dealer without going over.")
+    print("")
+    print("Cards:")
+    print(" - Ace = 1 or 11")
+    print(" - Face cards = 10")
+    print(" - Others = face value")
+    print("")
+    print("Your Turn:")
+    print(" - Hit = Take another card")
+    print(" - Stand = Keep your hand")
+    print("")
+    print("Win if:")
+    print(" - Your total > dealer's (without busting)")
+    print(" - Dealer busts")
+    print(" - Blackjack (Ace + 10) = instand 3:2 payout (unless dealer also has it)")
+    print("")
+    print("Lose if:")
+    print(" - You bust (go over 21)")
+    print(" - Dealer's total > yours")
+    print("===============================================================")
+    print("")
+
+elif Instructions == "no":
+    print("")
+    
+
+Start = Yes_No("Are you ready to start?\n")
+if Start == "yes":
+
+
+
+    print("")
+    print(f"You have ${Chips} in Chips")
+    Bet = int(input("How much would you like to bet? "))
+
+    Chips = Chips - Bet
+    print("")
+    print(f"You bet ${Bet}")
+    print(f"Remaining Chips are ${Chips}")
